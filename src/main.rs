86fddbb941b540e0320c7902007c6bc7bc9b344a
Ioan Andrasi <andrasi.ioan90@gmail.com>
2025-11-24 09:14:32 +0200

    Initialized logging to INFO

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use tracing::info;
+use crate::errors::AppError;
+
 mod database;
 mod errors;
 mod logic;
 mod models;
 mod routes;
 
-fn main() {
-    println!("Hello, world!");
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+    info!("Tracing initialized");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), AppError>{
+    init_tracing();
+    Ok(())
 }
